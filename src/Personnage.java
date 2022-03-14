b05e534e89bccecd58c31f523a3c318e2d6d04cb
Theo324 <101089313+Theo324@users.noreply.github.com>
2022-03-14 08:21:24 +0100

    Add files via upload

--- src/Personnage.java
+++ src/Personnage.java
@@ -1,11 +1,24 @@
-public class Personnage {
+public class Personnage extends Figurine{
+    public String classe;
     public String race;
-    public int[] stats = new int[6];
-    // 1 str 2 dex 3 int 4 cons 5 wis 6 char
+    public int HP;
+    public int HP_max;
+    public int For;
+    public int Mod_For;
+    public int Dex;
+    public int Mod_Dex;
+    public int Con;
+    public int Mod_Con;
+    public int Sag;
+    public int Mod_Sag;
+    public int Int;
+    public int Mod_Int;
+    public int Cha;
+    public int Mod_Cha;
 
 
-    public Personnage(){
-
+    public Personnage(String classchosen){
+        this.classe = classchosen;
     }
 
 
@@ -13,4 +26,12 @@ public class Personnage {
     public void setRace(String race){
         this.race = race;
     }
+
+    public void a1(){}  //re défintion des attaques dans chaque classe
+    public void a2(){}
+    public void a3(){}
+    public void a4(){}
+    public void a5(){}
+    public void a6(){}
+
 }
