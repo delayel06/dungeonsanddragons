0bc6d95b4313be29a22ec4167ad9725d42517ec5
tduca <theo.ducasse@insa-lyon.fr>
2022-03-25 12:38:14 +0100

    classe

--- src/Archer.java
+++ src/Archer.java
@@ -2,4 +2,13 @@ public class Archer extends Personnage {
     public Archer(){
 
     }
+    public void setStats(){
+        this.Str = 10 + this.stats[0];
+        this.Dex = 14 + this.stats[1];
+        this.Con = 13 + this.stats[2];
+        this.Int = 8 + this.stats[3];
+        this.Wis = 11 + this.stats[4];
+        this.Cha = 8 + this.stats[5];
+
+    }
 }
--- src/Mage.java
+++ src/Mage.java
@@ -16,5 +16,15 @@ public class Mage extends Personnage{
 
     }
 
+    public void setStats(){
+        this.Str = 8 + this.stats[0];
+        this.Dex = 11 + this.stats[1];
+        this.Con = 10 + this.stats[2];
+        this.Int = 14 + this.stats[3];
+        this.Wis = 13 + this.stats[4];
+        this.Cha = 8 + this.stats[5];
+
+    }
+
 
 }
