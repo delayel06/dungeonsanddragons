90725a084bdf59fc9c16d7af45ee4fbdcf9b4d56
tduca <theo.ducasse@insa-lyon.fr>
2022-04-03 14:15:44 +0200

    classe

--- src/Archer.java
+++ src/Archer.java
@@ -14,22 +14,22 @@ public class Archer extends Personnage {
     }
     public void a1(Monstre m){
         int n = (2+this.Mod_Dex);
-        texteA="Vous etes en symbiose avec la nature, récuperez "+n+" de vie";
+        texteA="Soins : Vous etes en symbiose avec la nature, récuperez "+n+" de vie";
         this.HP+=n;
     }
     public void a2(Monstre m){
         int n = (4+this.Mod_Dex);
-        texteA="Vous lancez un couteau dans le flan de la créature qui subit "+n+" points de vie";
+        texteA="Lancer habile : Vous lancez un couteau dans le flan de la créature qui subit "+n+" points de vie";
         m.HP-=n;
     }
     public void a3(Monstre m){
         int n = (6+this.Mod_Dex);
-        texteA="Vous effectuez une faite feinte et attaquez à la dague et infligez "+n+" dégats";
+        texteA="Changement de main : Vous effectuez une feinte main gauche et attaquez à la dague de la main droite infligeant "+n+" dégats";
         m.HP-=n;
     }
     public void a4(Monstre m){
         int n = (8+this.Mod_Dex);
-        texteA="Vous décochez une flèche qui touche et inflige "+n+" dégats";
+        texteA="Flèche du chasseur : Vous décochez une flèche qui touche et fait "+n+" dégats";
         m.HP-=n;
     }
 }
